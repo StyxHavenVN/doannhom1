cb1b82eaa534f9e3b5b1184be0b259aacf9039c0
kdoan <kdoan@Styx>
2024-10-02 23:49:38 +0700

    khanh

---
+++ src/main/java/com/mycompany/technology_app/Technology_App.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.technology_app;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Technology_App {
+
+    public static void main(String[] args) {
+        DnhapTechnology l = new DnhapTechnology();
+        l.setVisible(true);
+        
+    }
+}
